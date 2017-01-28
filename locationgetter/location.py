da5ea0853cb897f87c294e57b1cd084d4211b022
Emma <elh73@cam.ac.uk>
2017-01-28 18:40:06 +0000

    location getter

--- locationgetter/location.py
+++ locationgetter/location.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
 from api_keys import MICROSOFT_KEY
+
+########### Python 3.2 #############
+import requests
+
+headers = {
+    # Request headers
+    'Content-Type': 'text/plain',
+    'Ocp-Apim-Subscription-Key': MICROSOFT_KEY,
+}
+
+url = "https://westus.api.cognitive.microsoft.com/entitylinking/v1.0/link?"
+data = "Donald Trump is in London"
+
+r = requests.post(url, data=data, headers=headers)
+print(r.text)
+
+# check data includes Donald Trump? 
+
+
+# check data includes location
+
+####################################
