967525619566e9e031aceffbc9fbade5e36d7145
sofaw <sew56@cam.ac.uk>
2017-01-29 02:14:37 +0000

    Remove tests

--- locationgetter/get_furthest_airport.py
+++ locationgetter/get_furthest_airport.py
@@ -11,71 +11,10 @@ def parse_coords(json_coords):
 
     return latitude, longitude
 
-trump_location = (-8.581021, -54.118652) #TODO fix this
-
-antipode = (8.581021, 125.881348)
-manilla = (14.51, 121.018333)
 # get all airports
 all_url = "http://partners.api.skyscanner.net/apiservices/geo/v1.0?apiKey=" + SKYSCANNER_KEY
 all_airports = requests.get(all_url)
 airports_json = all_airports.json()
-
-
-num_continents = len(airports_json["Continents"])
-
-
-furthest_distance = 0
-furthest_airport = ""
-
-num_countries = 0
-num_cities = 0
-num_aiports = 0
-
-for continent in range(num_continents):
-	#print("Continent loop ------------------------------------------------------------------ ")
-	num_countries = len(airports_json["Continents"][continent]["Countries"])
-	#print(num_countries)
-	for country in range(num_countries):
-		#print("Country loop ------------------------------------------------------------------")
-		num_cities = len(airports_json["Continents"][continent]["Countries"][country]["Cities"])
-		#print(num_cities)
-		for city in range(num_cities):
-			#print("City loop ------------------------------------------------------------------")
-			num_aiports = len(airports_json["Continents"][continent]["Countries"][country]["Cities"][city]["Airports"])
-			#print(num_aiports)
-			for airport in range(num_aiports):
-				airport_coords = airports_json["Continents"][continent]["Countries"][country]["Cities"][city]["Airports"][airport]["Location"]
-				#print(airport_coords)
-				latitude, longitude = parse_coords(airport_coords)
-				#print(longitude)
-				#print(latitude)
-				airport_location = (longitude, latitude)
-
-				distance = great_circle(airport_location, trump_location).miles
-
-				if distance >= furthest_distance:
-					furthest_distance = distance
-					furthest_airport_name = airports_json["Continents"][continent]["Countries"][country]["Cities"][city]["Airports"][airport]["Name"]
-					furthest_airport_id = airports_json["Continents"][continent]["Countries"][country]["Cities"][city]["Airports"][airport]["Id"]
-
-'''
-for continent in range(num_continents):
-	#print("Continent loop ------------------------------------------------------------------ ")
-	num_countries = len(airports_json["Continents"][continent]["Countries"])
-	#print(num_countries)
-	for country in range(num_countries):
-		#print("Country loop ------------------------------------------------------------------")
-		print(json.dumps(airports_json["Continents"][continent]["Countries"][country], indent = 4))
-'''
-
-print(furthest_distance)
-print(furthest_airport_name)
-print(furthest_airport_id)
-
-print(great_circle(antipode, trump_location).miles)
-
-#TODO return furthest airport
-
 
 def find_airports(trump_loc, total_return=5):
     airport_dict = {}
